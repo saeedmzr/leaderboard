0ebb96106f7696f6f8cac7020d6f8f344b83331c
saeedmouzarmi <saeedmouzarmi@gmail.com>
2023-04-11 17:37:06 +0330

    add routes.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ScoreController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+
+        Route::get('get', [AuthController::class, 'get']);
+        Route::post('logout', [AuthController::class, 'logout']);
+
+
+    });
+
+});
+Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::post('/score', [ScoreController::class, 'store']);
+Route::get('/score/getTopList', [ScoreController::class, 'getTopList']);
+Route::get('/score/around', [ScoreController::class, 'getScoresAroundUser']);
 });
