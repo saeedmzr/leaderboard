48a257b5e449918d6728e7d288508103442be0b4
saeed <saeedmouzarmi@gmail.com>
2023-04-12 17:23:19 +0330

    update store score job.

--- app/Jobs/StoreScoreJob.php
+++ app/Jobs/StoreScoreJob.php
@@ -3,12 +3,12 @@
 namespace App\Jobs;
 
 use App\Repositories\Score\ScoreRepository;
-use App\Services\ScoreService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Redis;
 
 class StoreScoreJob implements ShouldQueue
 {
@@ -25,7 +25,7 @@ class StoreScoreJob implements ShouldQueue
         private ScoreRepository $scoreRepository,
         private int             $userId,
         array                   $payload,
-        private ScoreService    $scoreService,
+//        private ScoreService    $scoreService,
     )
     {
         $this->payload = $payload;
@@ -35,17 +35,21 @@ class StoreScoreJob implements ShouldQueue
      * Execute the job.
      *
      * @return void
+     * @throws \RedisException
      */
     public function handle()
     {
+        $redis = new Redis();
+        $redis->connect('localhost', 6379);
+
         $payload = $this->payload;
         $payload['user_id'] = $this->userId;
 
         $insertedScore = $this->scoreRepository->create($payload);
         $userHighestScore = $this->scoreRepository->user($this->userId)->getHighestScore()->first();
 
-        if ($insertedScore->id != $userHighestScore->id && $insertedScore->score > $userHighestScore) {
-            $this->scoreService->regenerateTopList();
+        if ($insertedScore->score > $redis->zScore('leaderboard', $this->userId)) {
+            $redis->zAdd('leaderboard', $insertedScore->score, $this->userId);
         }
     }
 }
