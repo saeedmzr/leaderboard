7a768a9203221aeeb7470356aabec5a8d89f2486
saeed <saeedmouzarmi@gmail.com>
2023-04-12 17:22:12 +0330

    change redis client .

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,30 +4,51 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\Auth\AuthResource;
-use App\Http\Resources\Auth\UserResource;
 use App\Http\Resources\SimpleResource;
 use App\Repositories\User\UserRepository;
+use Illuminate\Http\Request;
+use Redis;
+use RedisException;
 
 
 class AuthController extends Controller
 {
+    private Redis $redisClient;
 
+    /**
+     * @throws RedisException
+     */
     public function __construct(private UserRepository $userRepository)
     {
+        $this->redisClient = new Redis();
+        $this->redisClient->connect('localhost', 6379);
     }
 
     public function login(LoginRequest $loginRequest): SimpleResource|AuthResource
     {
         $user = $this->userRepository->login($loginRequest->email, $loginRequest->password);
-        if (!$user) return new SimpleResource(['message' => 'your credentials doesnt seems to be right.', 'status' => 422]);
+        if (!$user) {
+            return new SimpleResource(['message' => 'your credentials doesnt seems to be right.', 'status' => 422]);
+        }
 
         $token = $user->createToken('auth_token')->plainTextToken;
         return new AuthResource(['access_token' => $token]);
     }
 
-    public function get(): UserResource
+    /**
+     * @throws RedisException
+     */
+    public function get(Request $request): \Illuminate\Http\JsonResponse
     {
-        return new UserResource(auth()->user());
+        $userId = $request->user()->id;
+        $score = $this->redisClient->zScore('leaderboard', $userId);   // User's score
+        $rank = $this->redisClient->zRevRank('leaderboard', $userId) + 1; // User's rank
+
+        return response()->json([
+            'user' => $request->user(),
+            'score' => $score,
+            'rank' => $rank,
+        ]);
     }
 
     public function logout(): SimpleResource
