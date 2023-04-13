c40b697efd35d45730bb27cf2b0eafe2f67941ac
saeed <saeedmouzarmi@gmail.com>
2023-04-13 15:27:18 +0330

    add a method in baseTest.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function loginAsSaeed()
+    {
+        $user = User::where('email', 'saeedmouzarmi@gmail.com')->first();
+//        Auth::login($user);
+        $this->actingAs($user);
+        return $user;
+    }
+
 }
