2ff1ec3fcadf739745117f201c764442e805af9c
saeed <saeedmouzarmi@gmail.com>
2023-04-13 15:19:30 +0330

    updte contorller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Repositories\User\UserRepository;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,24 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    protected function generateScoreFromRedis(array $scores)
+    {
+        $userRepo = new UserRepository(new User());
+        $newScores = [];
+        foreach ($scores as $key => $score) {
+
+            $newScores[] = [
+                'user_id' => $key,
+                'score' => $score
+            ];
+        }
+        return collect($newScores)->map(function ($item) use ($userRepo) {
+            return [
+                'user' => $userRepo->findById($item['user_id']),
+                'score' => $item['score']
+            ];
+        });
+    }
 }
